5dccabe9ea7589d66d08ca95bce03f32b8d6a74a
ss8777 <103983790+ss8777@users.noreply.github.com>
2022-04-26 10:22:16 +0530

    Create srm-clap.c

---
+++ srm-clap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int d,h,m,s,t;
+scanf("%d",&t);
+    d=t/86400;
+    h=(t-(d*86400))/3600;
+    m=(t-(d*86400+h*3600))/60;
+    s=(t-(m*60+d*86400+h*3600));
+    printf("The Duration is %d days %d hours %d minutes %d seconds", d,h,m,s);
+    return 0;
+}
